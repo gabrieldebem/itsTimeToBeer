605b1fc1aaef3634885918d75e561b344e766d6b
gabrieldebem <gabrieldebem72@gmail.com>
2022-02-24 20:18:23 -0300

    remove cors

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express, { Application, Request, Response } from "express";
-import cors from "cors";
+
 
 const app: Application = express();
 const port = process.env.PORT || 5000;
@@ -8,16 +8,9 @@ const port = process.env.PORT || 5000;
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-const whiteList = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
-};
 
 app.get(
     "/",
-    cors(whiteList),
     async (req: Request, res: Response): Promise<Response> => {
         const value = Math.floor(Math.random() * 2);
 
